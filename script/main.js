45fd7e6d0c50034e8caf582ef640c0c424e4a16f
su apt install wayz <wayzzy59@gmail.com>
2023-01-28 13:50:53 +0100

    ajout reveal

--- script/main.js
+++ script/main.js
@@ -81,3 +81,21 @@ window.addEventListener('scroll', () => {
         topbtn.classList.remove('gotopbtn-scroll');
     }
 });
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((reveal) => {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal.getBoundingClientRect().top;
+      var elementVisible = 100;
+
+      if(elementTop < windowHeight - elementVisible) {
+          reveal.classList.add("active");
+      } else {
+          reveal.classList.remove("active");
+      }
+  });
+}
+
+window.addEventListener("scroll", reveal);
