e29184b93b92e9ff97d9f54936339bc42ed00f8a
su apt install wayz <wayzzy59@gmail.com>
2023-03-19 19:25:55 +0100

    ver 1.2.4

--- script/main.js
+++ script/main.js
@@ -39,3 +39,21 @@ const onScroll = (event) => {
 }
 
 document.addEventListener("scroll", onScroll);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    reveals.forEach((reveal) => {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveal.getBoundingClientRect().top;
+        var elementVisible = 100;
+  
+        if(elementTop < windowHeight - elementVisible) {
+            reveal.classList.add("active");
+        } else {
+            reveal.classList.remove("active");
+        }
+    });
+}
+  
+window.addEventListener("scroll", reveal);
